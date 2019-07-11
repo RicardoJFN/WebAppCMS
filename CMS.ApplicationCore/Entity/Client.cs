a718eb5158a2b9c670d03e7313d1c897f941a24b
Ricardo Jorge Nunes <rjorgenu@everis.com>
2019-07-11 13:53:20 +0100

    Add project files.

---
+++ CMS.ApplicationCore/Entity/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS.ApplicationCore.Entity
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long CCNumber { get; set; }
+
+        public Client()
+        {
+
+        }
+    }
+}
